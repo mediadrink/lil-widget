5cf39b07a4c7d71f5d27f28a35d724cf709d4a21
Jiah Sisco <jiahsisco@MacBook-Pro-7.local>
2026-01-13 12:41:33 -0800

    Remove widget embed from lilwidget.com homepage

--- app/page.tsx
+++ app/page.tsx
@@ -698,13 +698,6 @@ export default function LandingPage() {
         </div>
       )}
 
-      {/* Lil Widget - Dogfooding our own product on homepage! */}
-      <Script
-        src={`/widget.js?v=${Date.now()}`}
-        data-id="6a811b29-68e7-423c-9163-e7ef316af1b1"
-        data-base-url=""
-        strategy="lazyOnload"
-      />
     </div>
   );
 }
